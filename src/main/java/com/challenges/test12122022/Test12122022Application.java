2d9e5b58370e32e5fce4a6e6a0b592560903d300
Jorge Rodrigo Eudave Patiño <rodrigo.eudave@netlogistik.local>
2022-12-11 21:41:36 -0600

    first commit

---
+++ src/main/java/com/challenges/test12122022/Test12122022Application.java
@@ -0,0 +1,13 @@
+package com.challenges.test12122022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12122022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test12122022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenges/test12122022/Test12122022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenges.test12122022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12122022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
